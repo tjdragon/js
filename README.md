c770dab25982c557014a78481863b13957ab7670
tj <dragon@janaudy.com>
2016-12-13 17:29:48 +0800

    js1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # js
 JavaScript Stuff
+
+So fun with D3, JavaScript and some animation stuff
+JS feels like black magic to me...
--- script.js
+++ script.js
@@ -23,6 +23,18 @@ function tjCallback() {
 
     addPoint(myPoint);
 
+    if (p1 == 'undefined') p1 = myPoint;
+    else if (p2 == 'undefined') p2 = myPoint;
+    else {
+    	p1 = p2;
+    	p2 = myPoint;
+    }
+
+    if (p1 && p2) {
+    	drawLine(p1, p2);
+    	anim(p1, p2);
+    }
+
 	count = count + 1;
 }
 
@@ -41,11 +53,36 @@ function addPoint(tjPoint) {
 }
 
 function drawLine(from, to) {
+	const lines = d3.select('#svg-lines');
+	const lineId = 'l-' + from._id + '-' + to._id;
+	from.lineId = lineId;
 
+	lines.append('path')
+	     .attr('class', 'svg-line')
+	     .attr('id', lineId)
+	     .attr('d', 'M' + from._x + "," + from._y + ' L' + to._x + ',' + to._y);
 }
 
 function anim(from, to) {
-	
+	const walker = d3.select('#walker');
+	walker.style('display', 'block')
+	      .style('transform', `translate(${from._x}px, ${from._y}px)`);
+	walker.transition()
+	      .duration(interval * .9)
+	      .styleTween('transform', function() {
+	      	const interpolateX = d3.interpolate(from._x, to._x);
+	      	const interpolateY = d3.interpolate(from._y, to._y);
+	      	return function(t) {
+	      		return `translate(${interpolateX(t)}px, ${interpolateY(t)}px`;
+	      	};
+	      })
+	      .on("end", function() {
+	      	to.noreRef.classed('pulsing', true);
+	      	d3.select('#' + from._id).remove();
+	      	d3.select('#' + from.lineId).remove();	      	
+	      });
 }
 
-setTimeout(tjCallback, interval)
+// setTimeout(tjCallback, interval)
+// setTimeout(tjCallback, interval)
+setInterval(tjCallback, interval)
